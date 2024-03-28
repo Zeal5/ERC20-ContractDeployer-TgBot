82fa020a6a78eb1b4ec062e144164f820be02d60
root <root@vmi1757964.contaboserver.net>
2024-03-28 05:31:13 +0100

    add menu convo handler

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
+
+# internal imports
+from cogs.menu import menu_convo_handler
+
+# std imports 
 import os
 
 load_dotenv()
@@ -11,8 +16,14 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token(API).build()
+def main():
+    app = ApplicationBuilder().token(API).build()
+    app.add_handler(CommandHandler("hello", hello))
 
-app.add_handler(CommandHandler("hello", hello))
+    # add cogs here 
+    app.add_handler(menu_convo_handler,1)
 
-app.run_polling()
+    app.run_polling(0.1)
+
+if __name__ == "__main__":
+    main()
